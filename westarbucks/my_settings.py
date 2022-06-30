8a8551c0ca259a9d5f9a62a5060638d31a134400
WorkYong <kyong@WorkYongui-MacBookAir.local>
2022-06-30 21:05:43 +0900

    Add: products application

---
+++ westarbucks/my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Practice',
+        'USER': 'root',
+        'PASSWORD': 'Qwer1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-(k!k1j^mq!avxskax4okh2jfnq!%qdoc-t$e!#nqv)*l*rpb*e' #settings.py에 있는 
+
+my_settings.py
